0b14e8e122ad01944bfb6d73bbef2057833ee304
dlckdtjq153 <dlckdtjq153@gmail.com>
2022-09-08 09:35:34 +0900

    commit

--- crawling/siteCrainfo/Geuamcheoun.py
+++ crawling/siteCrainfo/Geuamcheoun.py
@@ -8,37 +8,39 @@ class Geuamcheoun:
     def mainCra(cnt,numberCnt):
         url = 'https://gcfac.or.kr/board/free';
         soupData = com.pageconnect(cnt, url, "javascript:pagingUtil.pageSubmit('{}')".format(cnt));
-        link = soupData.select('.title > a');
-        title = soupData.select('.title');
-        registrationdate = soupData.select('td:nth-child(5)');
+        
+        link = soupData.select('tr');
+        title = soupData.select('.title > a');
+        registrationdate = soupData.select('.title > .mVer > p:nth-child(2)');
 
-        print("link : ", link);
-        print("title : ", title);
-        print("registrationdate : ", registrationdate);
+        # print("link : ", link);
+        # print("registrationdate : ", registrationdate);
 
-        # linkCount = len(link) - 1;
-        # for i in range(len(link)):
-        #     numberCnt += 1;
-        #     if linkCount == i:
-        #         # javascript:pageNum('frm01','200');
-        #         cnt += 10;
-        #         print("Geuamcheoun Next Page : {}".format(cnt));
-        #         return Geuamcheoun.mainCra(cnt, numberCnt),
-        #     else:
-        #         if numberCnt == commonConstant_NAME.STOPCUOUNT:
-        #             break;
+        linkCount = len(link) - 1;
+        for i in range(len(link)):
+            numberCnt += 1;
+            if linkCount == i:
+                # javascript:pageNum('frm01','200');
+                cnt += 1;
+                print("Geuamcheoun Next Page : {}".format(cnt));
+                return Geuamcheoun.mainCra(cnt, numberCnt),
+            else:
+                if numberCnt == commonConstant_NAME.STOPCUOUNT:
+                    break;
+            linkSp = re.sub(r'[^0-9]','',link[i + 1].attrs.get('onclick'));
+            # print(title[i].text.strip());
+            print(registrationdate[i].text);
 
-        #     linkSp = re.sub(r'[^0-9]','',link[i].attrs.get('href'));
-        #     firebase_con.updateModel( commonConstant_NAME.GEUAMCHEOUN_NAME,i,
-        #         datasModel.toJson(
-        #             "https://gcfac.or.kr/board/freeDetail?notice_gb=&board_seq={}&gcfac_menu_cd=&currRow=1&scType=all&srch_input=".format(linkSp),
-        #             i,
-        #             "",
-        #             title[i].text.strip(),
-        #             "",
-        #             registrationdate[i + 1].text,
-        #             "금천문화재단"
-        #         )
-        #     )
+            firebase_con.updateModel( commonConstant_NAME.GEUAMCHEOUN_NAME,i,
+                datasModel.toJson(
+                    "https://gcfac.or.kr/board/freeDetail?notice_gb=&board_seq={}&gcfac_menu_cd=&currRow=1&scType=all&srch_input=".format(linkSp),
+                    i,
+                    "",
+                    title[i].text.strip(),
+                    "",
+                    registrationdate[i + 1].text,
+                    "금천문화재단"
+                )
+            )
 
             
