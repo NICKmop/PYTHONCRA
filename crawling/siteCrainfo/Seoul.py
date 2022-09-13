858bcf7980efae1415fa444c6e8636c0615bbd12
dlckdtjq153 <dlckdtjq153@gmail.com>
2022-09-13 13:24:26 +0900

    commit

--- crawling/siteCrainfo/Seoul.py
+++ crawling/siteCrainfo/Seoul.py
@@ -1,15 +1,19 @@
+from html.parser import HTMLParser
 import re
 from dbbox.firebases import firebase_con
 from common.common_constant import commonConstant_NAME
 from models.datasModel import datasModel
 import common.common_fnc  as com
-
+from bs4 import BeautifulSoup
+from selenium import webdriver
 class Seoul:
     def mainCra(cnt,numberCnt):
         url = 'https://www.sfac.or.kr/opensquare/notice/notice_list.do';
-        soupData = com.pageconnect(cnt, url, "doBbsFPag({});".format(cnt));
-        
+        script = "doBbsFPag(11)".format(cnt);
+        soupData = com.pageconnect(cnt, url, script);
+
         print(soupData);
+
         # link = soupData.select('.cell');
         # title = soupData.select('.cell');
         # registrationdate = soupData.select('td:nth-child(3)');
@@ -27,12 +31,12 @@ class Seoul:
         #             break;
         #     linkSp = link[i].attrs.get('onclick');
         #     print(linkSp)
-            # linkSp = link[i].attrs.get('onclick').split('Page');
-            # linkSub = re.sub(r'[^0-9]','',linkSp[0]);
+        #   linkSp = link[i].attrs.get('onclick').split('Page');
+        #   linkSub = re.sub(r'[^0-9]','',linkSp[0]);
             
-            # print(linkSub);
-            # print(title[i].text.strip());
-            # print(registrationdate[i].text);
+        #   print(linkSub);
+        #   print(title[i].text.strip());
+        #   print(registrationdate[i].text);
 
             # firebase_con.updateModel( commonConstant_NAME.SEOUL_NAME,numberCnt,
             #     datasModel.toJson(
