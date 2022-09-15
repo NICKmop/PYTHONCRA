6727ad9f142a45396b57e59d2f800da7a0564aae
dlckdtjq153 <dlckdtjq153@gmail.com>
2022-09-15 14:12:07 +0900

    commit

--- crawling/common/common_constant.py
+++ crawling/common/common_constant.py
@@ -1,47 +1,54 @@
 class commonConstant_NAME:
-    GANGNAM_NAME = "GANGNAM";           # 강남
-    GANGBUK_NAME ="GANGBUK";            # 강북
-    GWANAK_NAME = "GWANAK";             # 관악
-    GWANGZIN_NAME = "GWANGZIN";         # 광진
-    GEUAMCHEOUN_NAME = "GEUAMCHEOUN";   # 금천
-    DOBONG_NAME = "DOBONG";             # 도봉 -> page 크롤링 이슈상 잠깐 보류
-    DONGDAEMUN_NAME = "DONGDAEMUN";     # 동대문
-    DONGJAK_NAME = "DONGJAK";           # 동작 
-    MAPO_NAME = "MAPO";                 # 마포
-    SEOCHO_NAME = "SEOCHO";             # 서초 
-    SEONGDONG_NAME = "SEONGDONG";       # 성동
+    # 재단
+    GANGNAM_NAME = "GANGNAM";            # 강남
+    GANGBUK_NAME ="GANGBUK";             # 강북
+    GWANAK_NAME = "GWANAK";              # 관악
+    GWANGZIN_NAME = "GWANGZIN";          # 광진
+    GEUAMCHEOUN_NAME = "GEUAMCHEOUN";    # 금천
+    DOBONG_NAME = "DOBONG";              # 도봉 -> page 크롤링 이슈상 잠깐 보류
+    DONGDAEMUN_NAME = "DONGDAEMUN";      # 동대문
+    DONGJAK_NAME = "DONGJAK";            # 동작 
+    MAPO_NAME = "MAPO";                  # 마포
+    SEOCHO_NAME = "SEOCHO";              # 서초 
+    SEONGDONG_NAME = "SEONGDONG";        # 성동
     SEONGBUK_NAME = "SEONGBUK";          # 성북
     SONGPA_NAME = "SONGPA";              # 송파
     YANGCHEON_NAME = "YANGCHEON";        # 양천
     YEONGDEUNGPO_NAME = "YEONGDEUNGPO";  # 영등포 
     EUNPYENG_NAME = "EUNPYENG";          # 은평
-    JONGRO_NAME = "JONGRO";             # 종로
-    JUNGGU_NAME = "JUNGGU";             # 중구
+    JONGRO_NAME = "JONGRO";              # 종로
+    JUNGGU_NAME = "JUNGGU";              # 중구
+    NPO_NAME = "NPO";                    # NPO
+    SEOUL_NAME = "SEOUL";                # 서울
 
-    NPO_NAME = "NPO";               # NPO
-    SEOUL_NAME = "SEOUL";           # 서울
-
-    # 강남문화재단	    https://www.gangnam.go.kr/office/gfac/board/gfac_notice/list.do?mid=gfac_notice
-    # 강북문화재단	    http://www.gbcf.or.kr/load.asp?subPage=510
-    # 관악문화재단	    https://www.gfac.or.kr/html/notify/notify1.html
-    # 광진문화재단	    http://www.naruart.or.kr/bbs/board.php?bo_table=notice
-    # 구로문화재단	    https://www.guroartsvalley.or.kr/user/board/mn011801.do
-    # 금천문화재단	    https://gcfac.or.kr/board/free?gcfac_menu_cd=U0113
-    # 도봉문화재단	    http://www.dbfac.or.kr/front/board/boardContentsListPage.do?board_id=1
-    # 동대문문화재단    https://www.ddmac.or.kr/sub04/sub01.php
-    # 동작문화재단	    https://www.idfac.or.kr/bbs/board.php?bo_table=notice
-    # 마포문화재단	    https://www.mfac.or.kr/communication/notice_all_list.jsp
-    # 서초문화재단	    http://www.seochocf.or.kr/site/main/archive/post/category/%EA%B3%B5%EC%A7%80%EC%82%AC%ED%95%AD?metaCode1=GENERAL
-    # 성동문화재단	    https://www.sdfac.or.kr/kor/sdfac/board/noti_list.do?gotoMenuNo=06010000
-    # 성북문화재단	    https://www.sbculture.or.kr/culture/bbs/BMSR00021/list.do?menuNo=500049
-    # 송파문화재단	    https://www.songpafac.or.kr/notice_list.do
-    # 양천문화재단	    https://yfac.kr/main/contents.do?a_num=48342159
-    # 영등포문화재단	https://www.ydpcf.or.kr/board.do?bid=1
-    # 은평문화재단	    https://www.efac.or.kr/sub06/sub01.php
-    # 종로문화재단	    https://www.jfac.or.kr/site/main/archive/post/category/%EA%B3%B5%EC%A7%80%EC%82%AC%ED%95%AD
-    # 중구문화재단	    https://www.caci.or.kr/caci/bbs/BMSR00040/list.do?pageIndex=1&menuNo=200016&searchGubunCd=&searchCondition=&searchKeyword=
-    # 서울시NPO지원센터	https://www.snpo.kr/bbs/board.php?bo_table=bbs_npo
-    # 서울문화재단	    https://www.sfac.or.kr/opensquare/notice/notice_list.do
+    #자치 구
+    GANGNAM_BOROUGH_NOTICE = "";                    # 강남구
+    GANGNAM_BOROUGH_OTHER_INSTITUTIONS = "";        # 강남구
+    # 강남구		https://www.gangnam.go.kr/board/B_000001/list.do?mid=ID05_040101											https://www.gangnam.go.kr/board/B_000046/list.do?mid=ID05_0410
+    # 강동구		https://www.gangdong.go.kr/web/newportal/bbs/b_068															https://www.gangdong.go.kr/web/newportal/bbs/b_075
+    # 강북구		https://www.gangbuk.go.kr/www/boardList.do?boardSeq=41&key=285												https://www.gangbuk.go.kr/www/boardList.do?boardSeq=48&key=296
+    # 강서구		https://www.gangseo.seoul.kr/gs040101																		타기관소식없음	
+    # 관악구		https://www.gwanak.go.kr/site/gwanak/ex/bbs/List.do?cbIdx=239												https://www.gwanak.go.kr/site/gwanak/ex/bbs/List.do?cbIdx=239
+    # 광진구		https://www.gwangjin.go.kr/portal/bbs/B0000001/list.do?menuNo=200190										https://www.gwangjin.go.kr/portal/bbs/B0000006/list.do?menuNo=200195
+    # 구로구		https://www.guro.go.kr/www/selectBbsNttList.do?bbsNo=662&key=1790&											https://www.guro.go.kr/www/selectBbsNttList.do?bbsNo=669&key=1804&
+    # 금천구		https://www.geumcheon.go.kr/portal/selectBbsNttList.do?bbsNo=4&key=293										https://www.geumcheon.go.kr/portal/selectBbsNttList.do?bbsNo=100&key=296
+    # 노원구		https://www.nowon.kr/www/user/bbs/BD_selectBbsList.do?q_bbsCode=1001&q_estnColumn1=11						https://www.nowon.kr/www/user/bbs/BD_selectBbsList.do?q_bbsCode=1011
+    # 도봉구		https://www.dobong.go.kr/bbs.asp?code=10004124																https://www.dobong.go.kr/bbs.asp?code=10004281
+    # 동대문구	    https://www.ddm.go.kr/www/selectBbsNttList.do?bbsNo=38&key=198												https://www.ddm.go.kr/www/selectBbsNttList.do?bbsNo=43&key=209
+    # 동작구		https://www.dongjak.go.kr/portal/bbs/B0000022/list.do?menuNo=200641											https://www.dongjak.go.kr/portal/bbs/B0000174/list.do?menuNo=200643
+    # 마포구		https://www.mapo.go.kr/site/main/board/notice/list															https://www.mapo.go.kr/site/main/board/othernews/list
+    # 서대문구	    https://www.sdm.go.kr/news/news/notice.do																	https://www.sdm.go.kr/news/news/another.do
+    # 서초구		https://www.seocho.go.kr/site/seocho/ex/bbs/List.do?cbIdx=57												https://www.seocho.go.kr/site/seocho/ex/bbs/List.do?cbIdx=69
+    # 성동구		https://www.sd.go.kr/main/selectBbsNttList.do?bbsNo=183&key=1472&											https://www.sd.go.kr/main/selectBbsNttList.do?bbsNo=186&key=1475&
+    # 성북구		https://www.sb.go.kr/PageLink.do																			https://www.sb.go.kr/PageLink.do
+    # 송파구		https://www.songpa.go.kr/www/selectBbsNttList.do?bbsNo=92&key=2775&											https://www.songpa.go.kr/www/selectBbsNttList.do?bbsNo=95&key=2780&
+    # 양천구		https://www.yangcheon.go.kr/site/yangcheon/ex/bbs/List.do?cbIdx=254											타기관소식없음
+    # 영등포구	    https://www.ydp.go.kr/www/selectBbsNttList.do?bbsNo=40&key=2848&											https://www.ydp.go.kr/www/selectBbsNttList.do?bbsNo=47&key=2869&
+    # 용산구		https://www.yongsan.go.kr/portal/bbs/B0000041/list.do?menuNo=200228											https://www.yongsan.go.kr/portal/bbs/B0000046/list.do?menuNo=200236
+    # 은평구		https://www.ep.go.kr/www/selectBbsNttList.do?bbsNo=42&key=744												https://www.ep.go.kr/www/selectBbsNttList.do?bbsNo=53&key=771
+    # 종로구		https://www.jongno.go.kr/portal/bbs/selectBoardList.do?bbsId=BBSMSTR_000000000201&menuId=1752&menuNo=1752	https://www.jongno.go.kr/portal/bbs/selectBoardList.do?bbsId=BBSMSTR_000000000231&menuId=1758&menuNo=1758
+    # 중구		    http://www.junggu.seoul.kr/content.do?cmsid=14231															타기관소식없음
+    # 중랑구		https://www.jungnang.go.kr/portal/bbs/list/B0000002.do?menuNo=200473										https://www.jungnang.go.kr/portal/bbs/list/B0000128.do?menuNo=200478
 
     STOPCUOUNT = 21;
     STATUS_SUCCESS_CODE = 200;
